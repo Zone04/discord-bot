84fa813ed39f04a784c4f0f4fcc2776da8a96464
Enzo <enzod@student.chalmers.se>
2020-08-04 23:09:56 +0200

    Updated update message

--- commands/update.js
+++ commands/update.js
@@ -3,20 +3,21 @@ module.exports = {
     description: 'Fait un git pull sur la branch master',
     execute(message, args) {
         const { exec } = require("child_process")
-        message.channel.send('Executing git pull');
-        exec('git pull', (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
-            if (stdout) {
-                console.log(`stdout: ${stdout}`);
-                message.channel.send('Done!');
-            }
+        message.channel.send('Mise à jour en cours.').then(m => {
+            exec('git pull', (error, stdout, stderr) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    return;
+                }
+                if (stderr) {
+                    console.log(`stderr: ${stderr}`);
+                    return;
+                }
+                if (stdout) {
+                    console.log(`stdout: ${stdout}`);
+                }
+            });
+
         });
     },
 };
