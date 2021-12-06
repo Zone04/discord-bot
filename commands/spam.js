04bdb6d8fc30922593b05c276a324899a4456b9b
Zone04 <aenzodandrea@gmail.com>
2021-12-06 19:25:14 +0100

    Improve spam performances + progress

--- commands/spam.js
+++ commands/spam.js
@@ -6,11 +6,18 @@ module.exports = {
     args: true,
     execute(message, args) {
         utils.convertUser(message, args[0]).then(user => {
-            for (let i = 0; i < args[1]; i++) {
-                message.channel.send(`${user}`);
-            }
+            spam(message.channel, user, args[1], args[1])
         }).catch (e => {
             message.channel.send('Erreur : ' + e);
         });
     },
 };
+
+function spam(channel, user, count, total) {
+    if (count > 0) {
+        count--;
+        channel.send(`${user}, ${count} pings restant sur ${total}`).then(() => {
+            spam(channel, user, count, total);
+        });
+    }
+}
