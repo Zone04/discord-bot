cc53b294e424ababcf41272083147564bc7edef0
Zone04 <aenzodandrea@gmail.com>
2021-12-16 17:24:05 +0100

    added settings for remaining commands

--- commands/pong.js
+++ commands/pong.js
@@ -1,6 +1,15 @@
-module.exports = {
+let settings = {
     name: 'pong',
-    description: 'Ping MDR',
+    description: 'ping ?',
+    args: false,
+    usage: '',
+}
+
+module.exports = {
+    name: settings.name,
+    description: settings.description,
+    args: settings.args,
+    usage: settings.usage,
     execute: async(message, args) => {
         message.channel.send('Ping MDR :joy:');
     },
