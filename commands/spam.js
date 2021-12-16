798e02d37a56b804bcf26d4960d695d29832a3f8
Zone04 <aenzodandrea@gmail.com>
2021-12-16 03:03:01 +0100

    Check args spam

--- commands/spam.js
+++ commands/spam.js
@@ -3,8 +3,17 @@ const utils = require('../utils.js');
 module.exports = {
     name: 'spam',
     description: 'Spamme un utilisateur.',
-    args: true,
     execute: async (message, args) => {
+        if (args.length < 2) {
+            return message.channel.send(`Vous devez donner un utilisateur et un nombre de pings ${message.author}`);
+        }
+        if (isNaN(args[args.length - 1]) || parseInt(args[args.length - 1]) <= 0) {
+            return message.channel.send(`Vous devez donner un nombre de pings à envoyer ${message.author}`);
+        }
+        if (parseInt(args[args.length - 1]) > 100000) {
+            return message.channel.send(`Tu abuserais pas un peu là ${message.author} ? Je suis raisonnable moi, je fais pas plus de 100000 pings d'un coup`);
+        }        
+
         guildMember = await utils.convertUser(message, args.slice(0,-1).join(' '))
 
         for (let i = 1; i <= parseInt(args[args.length - 1]); i++) {
