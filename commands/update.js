cdd180d0589f8d73682c3e63af20d275e664fe03
Zone04 <aenzodandrea@gmail.com>
2021-12-16 03:09:24 +0100

    Disabled update command

--- commands/update.js
+++ commands/update.js
@@ -1,23 +1,7 @@
 module.exports = {
     name: 'update',
     description: 'Fait un git pull sur la branch master',
-    execute(message, args) {
-        const { exec } = require("child_process")
-        message.channel.send('Mise à jour en cours.').then(m => {
-            exec('git pull', (error, stdout, stderr) => {
-                if (error) {
-                    console.log(`error: ${error.message}`);
-                    return;
-                }
-                if (stderr) {
-                    console.log(`stderr: ${stderr}`);
-                    return;
-                }
-                if (stdout) {
-                    console.log(`stdout: ${stdout}`);
-                }
-            });
-
-        });
+    execute: async(message, args) => {
+        await message.channel.send('Commande désactivée pour le moment')
     },
 };
