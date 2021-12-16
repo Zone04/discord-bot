6f63986f89f0fc2a7a230ecf1ae5ca98f976ed98
Zone04 <aenzodandrea@gmail.com>
2021-12-16 02:07:13 +0100

    Updated clear command

--- commands/clear.js
+++ commands/clear.js
@@ -1,9 +1,14 @@
 module.exports = {
     name: 'clear',
     description: 'Clear a specified number of messages',
-    execute(message, args) {
-        message.channel.messages.fetch({ limit: args[0] }).then(messages => {
-            message.channel.bulkDelete(messages)
-        });
+    execute: async(message, args) => {
+        if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
+            return message.channel.send(`Vous devez donner un nombre de messages à supprimer ${message.author}`);
+        }
+
+        let toDelete = parseInt(args[0]) > 99 ? 100 : parseInt(args[0]) + 1;
+
+        messages = await message.channel.messages.fetch({ limit: toDelete });
+        await message.channel.bulkDelete(messages, true);
     },
 };
