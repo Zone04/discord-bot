e69e5abe6fb5cc9172b1683ea2474dfae613c1bb
Zone04 <aenzodandrea@gmail.com>
2021-12-19 15:31:26 +0100

    Reduce max ping

--- commands/spam.js
+++ commands/spam.js
@@ -20,8 +20,8 @@ module.exports = {
         if (isNaN(args[args.length - 1]) || parseInt(args[args.length - 1]) <= 0) {
             return message.channel.send(`Usage: \`${prefix}${settings.name} ${settings.usage}\``);
         }
-        if (parseInt(args[args.length - 1]) > 100000) {
-            return message.channel.send(`Tu abuserais pas un peu là ${message.author} ? Je suis raisonnable moi, je fais pas plus de 100000 pings d'un coup`);
+        if (parseInt(args[args.length - 1]) > 10000) {
+            return message.channel.send(`Tu abuserais pas un peu là ${message.author} ? Je suis raisonnable moi, je fais pas plus de 10000 pings d'un coup`);
         }        
 
         let content;
