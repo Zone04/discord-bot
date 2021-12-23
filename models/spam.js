a00a5b9da4f66d5a261ed297bff2b17100fdd7c1
Zone04 <aenzodandrea@gmail.com>
2021-12-24 00:39:33 +0100

    create spam table

---
+++ models/spam.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Spam extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Spam.init({
+    source: DataTypes.STRING,
+    target: DataTypes.STRING,
+    number: DataTypes.INTEGER,
+    progress: DataTypes.INTEGER,
+    channel: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Spam',
+  });
+  return Spam;
+};
