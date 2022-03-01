782bf918eae850e9eb89cf812cb2fa25ad7ced68
Zone04 <aenzodandrea@gmail.com>
2022-03-01 18:06:02 +0100

    Create Settings

---
+++ database/migrations/20220301163829-create-setting.js
@@ -0,0 +1,26 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Settings', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING
+      },
+      spamLimit: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Settings');
+  }
+};
