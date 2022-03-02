d27d04a57a514319b1515acf06a56d6c49ce4f63
Zone04 <aenzodandrea@gmail.com>
2022-03-02 21:36:32 +0100

    Check error type

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,4 +1,5 @@
 const utils = require('../utils.js');
+const UserNotFoundError = require('../errors/UserNotFoundError.js');
 
 let settings = {
     name: 'userinfo',
@@ -24,8 +25,18 @@ module.exports = {
         } else {
             user = args.join(' ');
         }
-
-        let guildMember = await utils.convertUser(message, user)
+        
+        let guildMember;
+        try {
+            guildMember = await utils.convertUser(message, user);
+        } catch(e) {
+            if (e instanceof UserNotFoundError) {
+                message.reply(e.message);
+                return;
+            } else {
+                throw e;
+            }
+        } 
 
         message.channel.send(`L'utilisateur s'appelle \`\`${guildMember.user.tag}\`\`\nStatut : \`\`${guildMember.presence?.status ?? 'offline'}\`\`\nCompte créé le \`\`${guildMember.user.createdAt}\`\``);
     },
--- errors/UserNotFoundError.js
+++ errors/UserNotFoundError.js
@@ -1,6 +1,6 @@
-module.exports = class MyError extends Error {
+module.exports = class UserNotFoundError extends Error {
     constructor(message) {
         super(message);
-        this.name = 'MyError';
+        this.name = 'UserNotFoundError';
     }
 }
