6f76584270dd179fa06b0fef07cb6a95c4f83e49
Zone04 <aenzodandrea@gmail.com>
2022-06-02 01:12:34 +0200

    add lingerie

---
+++ commands/NSFW/lingerie.js
@@ -0,0 +1,52 @@
+const utils = require('../../utils.js');
+const axios = require('axios');
+const UserNotFoundError = require('../../errors/UserNotFoundError.js');
+
+let settings = {
+    name: 'lingerie',
+    description: 'De belles lingeries...',
+    usage: [
+        {
+            name: 'NOMBRE',
+            description: 'Nombre de photos, default 1, max 5',
+            optional: true
+        }
+    ],
+}
+
+module.exports = {
+    name: settings.name,
+    description: settings.description,
+    usage: settings.usage,
+    permitted: (client, message) => {
+        return message.channel.nsfw;
+    },
+    check_args: (message, args) => {
+        if (args.length == 0) { return true; }
+        return args.length == 1 && !isNaN(args[0]) && parseInt(args[0]) <= 5 && parseInt(args[0]) >= 1;
+    },
+    execute: async(message, args) => {
+        let number = 1;
+        
+        let res = await axios.get('https://reddit.com/r/lingerie/top.json?limit=100');
+        
+        if (args.length) {
+            number = Math.min(parseInt(args[0]), res.data.data.children.length);
+        }
+        let bucket = [];
+        for (let i=0;i<=res.data.data.children.length;i++) {
+            bucket.push(i);
+        }
+        function getRandomFromBucket() {
+            var randomIndex = Math.floor(Math.random()*bucket.length);
+            return bucket.splice(randomIndex, 1)[0];
+        }
+
+        let ans = '';
+        for (let i=0;i<number;i++) {
+            ans += res.data.data.children[getRandomFromBucket()].data.url + `\n`;
+        }
+
+        message.channel.send(ans);
+    },
+};
